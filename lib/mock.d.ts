3dea3f856a95f012f71038432b61dca5d5feee5f
rad12000 <chadbekmezian@gmail.com>
2022-07-16 22:03:57 -0600

    Build

--- lib/mock.d.ts
+++ lib/mock.d.ts
@@ -3,9 +3,9 @@ declare class Mock<T> {
     private readonly propertyConfigMap;
     private readonly _object;
     constructor();
-    getCallCount: (method: keyof T) => number;
-    resetCallCount: () => void;
-    object: () => T;
+    getCallCount: (property: keyof T) => number;
+    resetCallCount(): void;
+    object(): T;
     setupMethod<K extends keyof T>(methodName: K): ConfigureReturn<T, K>;
     setupField<K extends keyof T>(fieldName: K): ConfigureValue<T, K>;
 }
